b7dd0bb89d2592ccd922a04badceddcb791ff9b8
Patrick Nunes <patrick.n.souza@outlook.com>
2023-06-25 12:55:26 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const uploadButton = document.getElementById("upload-button");
+const chosenImage = document.getElementById("chosen-image");
+const fileName = document.getElementById("file-name");
+const container = document.querySelector(".container");
+const error = document.getElementById("error");
+const imageDisplay = document.getElementById("image-display");
+
+
+const fileHandler = (file,name,type) =>{
+
+    if (type.split("/")[0] !== "image"){
+        //File Type errors
+        error.innerText = "Please upload an image file";
+        return false;
+    }
+    error.innerText = "";
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+        // image and file name
+        let imageContainer = document.createElement("figure");
+        let img = document.createElement("img");
+        img.src = reader.result;
+        imageContainer.appendChild(img);
+        imageContainer.innerHTML +=`<figcaption>${name}</figcaption>`;
+        imageDisplay.appendChild(imageContainer);
+    };
+};
+
+uploadButton.addEventListener("change", () => {
+    imageDisplay.innerHTML="";
+    Array.from(uploadButton.files).forEach((file) =>{
+        fileHandler(file , file.name , file.type);
+    });
+});
+
+container.addEventListener("dragenter", (e) =>{
+    e.preventDefault();
+    e.stopPropagation();
+    container.classList.add("active");
+}, false);
+
+container.addEventListener("dragleave", (e) =>{
+    e.preventDefault();
+    e.stopPropagation();
+    container.classList.remove("active");
+},false);
+
+container.addEventListener("dragover", (e) =>{
+    e.preventDefault();
+    e.stopPropagation();
+    container.classList.add("active");
+},false);
+
+container.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    container.classList.remove("active");
+    let draggedData = e.dataTransfer;
+    let files = draggedData.files;
+    imageDisplay.innerHTML = ""
+    Array.from(files).forEach((file) =>{
+        fileHandler(file , file.name , file.type);
+    });
+    
+},false);
